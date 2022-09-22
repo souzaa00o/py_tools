ed6f158a3d293011e8707d8e11cd6e86844ab86b
souzaa00o <henriqueskts991@gmail.com>
2022-09-22 14:38:49 -0300

    Done

--- scripts_aws/check_lambda_policy/get_attached_role_policies.py
+++ scripts_aws/check_lambda_policy/get_attached_role_policies.py
@@ -1,87 +1,97 @@
 import resource
+
 import boto3
 
-def main():
-    cf = boto3.client("cloudformation")
-    lf = boto3.client("lambda")
-    iamf = boto3.client("iam")
-    stacks_names = get_stacks_functions(cf)
-    stack_resources = get_stack_resource(stacks_names, cf)
-    role_resources = get_function_configuration(lf, stack_resources)
-    attached_policies = get_attached_role_policies(iamf, role_resources)
 
-    print(attached_policies)
+def list_functions():
+    cf_cli = boto3.client("cloudformation")
+    stack_names = list_stack_names(cf_cli)
 
+    functions = []
 
-def get_stacks_functions(cf):
-    stacks = cf.list_stacks(StackStatusFilter=['CREATE_COMPLETE','UPDATE_COMPLETE','ROLLBACK_COMPLETE','DELETE_FAILED'])
-    stacks_names = [stack["StackName"] for stack in stacks["StackSummaries"]]
+    for stack_name in stack_names:
+        resources = cf_cli.list_stack_resources(StackName=stack_name)
 
-    while stacks.get("NextToken"):
-        stacks = cf.list_stacks(NextToken=stacks["NextToken"])
-        stacks_names.append([stack["StackName"] for stack in stacks["StackSummaries"]])
-
-    return stacks_names
-
-
-def get_stack_resource(stacks_names, cf):
-    fname_result = []
-
-    for stack in stacks_names:
-        resources = cf.list_stack_resources(StackName=stack)
-
-        for resource in resources["StackResourceSummaries"]:    
+        for resource in resources["StackResourceSummaries"]:
             if resource["ResourceType"] == "AWS::Lambda::Function":
                 function_info = {
-                    "stack_name": stack,
-                    "function_name": resource["PhysicalResourceId"]
+                    "stack": stack_name,
+                    "function": resource["PhysicalResourceId"],
                 }
 
-                fname_result.append(function_info)
+                functions.append(function_info)
 
-    return fname_result
+    return functions
 
 
-def get_function_configuration(lf, stack_resources):
-    # rname_result = []
+def list_stack_names(cf_cli):
+    stacks = cf_cli.list_stacks()
+    stack_names = []
+    invalid_status = ["DELETE_COMPLETE", "DELETE_IN_PROGRESS"]
 
-    for stack in stack_resources:
-        function_name = stack["function_name"]
+    for stack in stacks["StackSummaries"]:
+        if stack["StackStatus"] not in invalid_status:
+            stack_names.append(stack["StackName"])
+
+    while stacks.get("NextToken"):
+        stacks = cf_cli.list_stacks(NextToken=stacks["NextToken"])
+        for stack in stacks["StackSummaries"]:
+            if stack["StackStatus"] not in invalid_status:
+                stack_names.append(stack["StackName"])
+
+    return stack_names
+
+
+def list_role_functions(functions):
+    lambda_cli = boto3.client("lambda")
+
+    roles = []
+
+    for function in functions:
         try:
-            configuration = lf.get_function_configuration(FunctionName=function_name)
+            configuration = lambda_cli.get_function_configuration(FunctionName=function["function"])
         except Exception:
             continue
 
-        stack['role'] = configuration["Role"].split('/')[1]
+        role = configuration.get("Role", "").split("/", maxsplit=1)
 
-        # role_info = {
-        #     "stack_name": stack["stack_name"],
-        #     "function_name": function_name,
-        #     "role": configuration["Role"].split('/')[1]
-        # }
-            
-        # rname_result.append(role_info)
-    
-    return stack_resources
+        try:
+            role = role[1]
+        except IndexError:
+            continue
 
-def get_attached_role_policies(iamf, role_resources):
-    pname_result = []
-    
-    for role in role_resources:
+        role = {"role": role, **function}
+        roles.append(role)
+
+    return roles
+
+
+def list_function_policies():
+    functions = list_functions()
+    roles = list_role_functions(functions)
+
+    iam = boto3.client("iam")
+
+    result = []
+
+    for role in roles:
         role_name = role.get("role")
-        try:
-            policy = iamf.list_attached_role_policies(RoleName=role_name)
-        except Exception:
-            continue
-        
-        for policy in policy["AttachedPolicies"]:
-            pname_result.append({
-                "stack_name": role["stack_name"],
-                "function_name": role["function_name"],
-                "policies": policy
-            })
-    print(pname_result)
-    return pname_result
 
+        if role_name:
+            try:
+                policies = iam.list_attached_role_policies(RoleName=role_name)
+            except Exception:
+                continue
 
-main()
+            for policy in policies["AttachedPolicies"]:
+                result.append(
+                    {
+                        "stack": role["stack"],
+                        "function": role["function"],
+                        "policy": policy["PolicyName"],
+                    }
+                )
+
+    return result
+
+list_functions()
